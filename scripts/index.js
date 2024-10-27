c063a85bc4453226085a3048dd6b78f1d1e03647
Johnnathon Williams <jwill52492@gmail.com>
2024-10-27 11:35:02 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -72,5 +72,5 @@ editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 for (let i = 0; i < initialCards.length; i++) {
   const cardElement = getCardElement(initialCards[i]);
-  cardsList.prepend(cardElement);
+  cardsList.append(cardElement);
 }
